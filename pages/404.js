0b0fb4ee044efcefe1a85059f15c1ce7b0025050
Wildan Novaldi <wildanv10@gmail.com>
2022-09-10 22:23:15 +0700

    Add link to home in the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import style from "./404.module.css";
+import utilStyles from "./../styles/utils.module.css";
 
 export default function Custom404() {
   return (
     <div className={style.container}>
       <h1>404 - Page Not Found</h1>
+      <Link href="/">
+        <a className={utilStyles.colorInherit}>Back to Home</a>
+      </Link>
     </div>
   );
 }
